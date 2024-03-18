e8ba2b95a1bdb1d39adea30ec18ef1669ceeefbf
juno5626 <junono1@naver.com>
2024-03-18 23:45:58 +0900

    threedice code1\

---
+++ 조건문/three dice.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int di1,di2,di3;
+    
+    scanf("%d %d %d",&di1,&di2,&di3);
+
+    if(di1 == di2 && di2 == di3){
+        printf("%d",10000+di1*1000);
+    }else if()
+}
