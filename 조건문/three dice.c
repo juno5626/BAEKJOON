eed8193f7b5912064aa7fb0870e93ac6b3a4f804
juno5626 <junono1@naver.com>
2024-03-19 10:35:30 +0900

    three dice code 1(error exist

--- 조건문/three dice.c
+++ 조건문/three dice.c
@@ -7,7 +7,27 @@ int main(){
 
     if(di1 == di2 && di2 == di3){
         printf("%d",10000+di1*1000);
-    }else if()
+    }else if(di1 != di2 && di2 != di3){
+        if(di1>di2){
+            if(di1>di3){
+                printf("%d",100*di1);
+            }else{
+                printf("%d",100*di3);
+            }
+        }else{
+            if(di2>di3){
+                printf("%d",100*di2);
+            }else{
+                printf("%d",100*di3);
+            }
+        }
+    }else{
+        if(di1==di2||di1==di3){
+            printf("%d",1000+di1*100);
+        }else{
+            printf("%d",1000+di2*100);
+        }
+    }
 
     
 }
