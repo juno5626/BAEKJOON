34e7344e1e0b6104b2f8c40ec22c8bada6ceb2b8
juno5626 <junono1@naver.com>
2024-03-31 16:01:36 +0900

    balls

---
+++ 1차원 배열/balls.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+    int N, M;
+    int i , j , num;
+    scanf("%d %d",&N, &M);
+    int a[N];
+    for(int w = 0 ; w<N ; w++){
+        a[w] = 0;
+    }
+    for(int q = 0 ; q<M ;q++){
+        scanf("%d %d %d",&i, &j,&num);
+
+        for(int o = i-1 ; o<=j-1 ;o++){
+            a[o]=num;
+        }   
+    }
+    for(int w = 0 ; w<N ; w++){
+        printf("%d ",a[w]);
+    }
+
+}
