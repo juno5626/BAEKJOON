b9070d3eace24bb250462c2336e94d71619813f6
juno5626 <junono1@naver.com>
2024-04-07 02:26:47 +0900

    2675 문자열 반복 정답

---
+++ 문자열/repeatString.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(){
+    int t, r;
+    int count=0;
+    char s[21];
+    char a[80];
+    scanf("%d",&t);
+    char* p[1000];
+
+
+    for(int i = 0 ; i < t ; i++){
+        scanf("%d",&r);
+        scanf("%s",s);
+        
+        p[i]=(char*)malloc(strlen(s)+strlen(s)+strlen(s)+strlen(s)+strlen(s)+strlen(s)+strlen(s)+strlen(s));
+
+
+        for(int o = 0 ; o < strlen(s) ;o++){
+            for(int k = 0 ; k < r ; k++){
+                p[i][count] = s[o];
+                count++;
+            }
+        }
+        count=0;
+    }
+
+    for(int i = 0 ; i<t ; i++){
+        printf("%s\n",p[i]);
+    }
+
+
+    return 0;
+}
